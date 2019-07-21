d5e6b914d9b713bc10c8cbc824abe18bc6a8b279
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-07-21 07:35:31 +0530

    fix+rename: Improved menuconfig.sh, fixed cond. bug

--- utils/menuconfig.sh
+++ utils/menuconfig.sh
@@ -7,12 +7,13 @@ x86="< # "
 
 function build_config() {
   echo "[+] Running the build process ..."
+  cp -f base-firmware.config /tmp/base-firmware.config
   while read -r line; do
    if [[ "$(echo $line | head -c 1)" == $2 ]];
    then
     line=$(tr -s $1 '@' <<< "$line")
     line=$(cut -d "@" -f 2 <<< "$line")
-    echo -e "$line" >> base-firmware.config
+    echo -e "$line" >> /tmp/base-firmware.config
     echo "[*] Written '$line' to base-firmware.config"
    fi
   done < "$3"
@@ -25,7 +26,7 @@ echo "        2. Raspberry Pi (Broadcom BCM27xx)"
 read -p "Enter number to choose arch build type (i.e. 1, 2): " opt
 read -p "    Continue build? (Y/N): " \
     confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] \
-    || echo "Script aborted. Quitting." && exit 0
+    || { echo "Script aborted. Quitting."; exit 0; }
 printf "\n\n"
 if [[ $opt = "1" ]]
 then
@@ -39,4 +40,5 @@ then
 else
   echo "[-] Invalid input."
 fi
-printf "\n\n[+] Configuration successfully written.\n"
+mv -f /tmp/base-firmware.config OpenWrt/openwrt-18.06.2/.config
+printf "\n\n[+] Generated the .config.\n[*] Moving to target location.\n[+] Configuration successfully written.\n"

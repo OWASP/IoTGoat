f3edab664375bbeb35809f62f63154a5b15f5f2a
Tom Isaacson <parsley72@gmail.com>
2024-10-19 11:20:27 +1300

    build: fix checks for GCC11
    From https://github.com/openwrt/openwrt/commit/cae69d558135456976b8fc6cb08530d1358cf6d5

--- OpenWrt/openwrt-18.06.2/include/prereq-build.mk
+++ OpenWrt/openwrt-18.06.2/include/prereq-build.mk
@@ -28,8 +28,8 @@ $(eval $(call TestHostCommand,proper-umask, \
 
 $(eval $(call SetupHostCommand,gcc, \
 	Please install the GNU C Compiler (gcc) 6 or later, \
-	$(CC) -dumpversion | grep -E '^([6-9]\.?|10\.?)', \
-	gcc -dumpversion | grep -E '^([6-9]\.?|10\.?)', \
+	$(CC) -dumpversion | grep -E '^([6-9]\.?|1[0-9]\.?)', \
+	gcc -dumpversion | grep -E '^([6-9]\.?|1[0-9]\.?)', \
 	gcc --version | grep -E 'Apple.(LLVM|clang)' ))
 
 $(eval $(call TestHostCommand,working-gcc, \
@@ -40,8 +40,8 @@ $(eval $(call TestHostCommand,working-gcc, \
 
 $(eval $(call SetupHostCommand,g++, \
 	Please install the GNU C++ Compiler (g++) 6 or later, \
-	$(CXX) -dumpversion | grep -E '^([6-9]\.?|10\.?)', \
-	g++ -dumpversion | grep -E '^([6-9]\.?|10\.?)', \
+	$(CXX) -dumpversion | grep -E '^([6-9]\.?|1[0-9]\.?)', \
+	g++ -dumpversion | grep -E '^([6-9]\.?|1[0-9]\.?)', \
 	g++ --version | grep -E 'Apple.(LLVM|clang)' ))
 
 $(eval $(call TestHostCommand,working-g++, \
